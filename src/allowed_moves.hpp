2452ecce364ebbf8e4df28457fb5746d40136d47
Your Name <your-email@example.com>
2026-08-08 21:34:44 -0400

    added allowed moves

--- src/allowed_moves.hpp
+++ src/allowed_moves.hpp
@@ -1,139 +1,278 @@
 #include "headers.hpp"
+// std::vector<int> find_moves(std::vector<std::vector<int>> chess_board,int position,int type);
+std::vector<int> find_moves(std::vector<std::vector<int>> &chess_board, int position, int type)
+{
+    int i = position / 10;
+    int j = position % 10;
+    std::vector<int> allowed_moves;
 
-void check(std::unordered_map<int, std::vector<int>> &allowed_moves, std::vector<std::vector<int>> chess_board){
-    for(int i=0;i<chess_board.size();i++){
-        for(int j=0;j<chess_board[0].size();j++){
-            if(chess_board[i][j]==0)continue;
-            find_moves(allowed_moves, chess_board,((i*10)+j),(chess_board[i][j]));
+    if (type == 12)
+    {
+        if (i > 0)
+            i++;
+        allowed_moves.push_back(((i * 10) + j));
+    }
+    if (type == 11)
+    {
+        if (i < 8)
+            i--;
+        allowed_moves.push_back(((i * 10) + j));
+    }
+
+    if (type == 42 || type == 41)
+    {
+        i += 1;
+        j += 1;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+        {
+            if ((chess_board[i][j] % 10) != type % 10 && chess_board[i][j] % 10 != 0)
+            {
+                allowed_moves.push_back(((i * 10) + j));
+                break;
+            }
+            allowed_moves.push_back(((i * 10) + j));
+            i += 1;
+            j += 1;
+        }
+        i = position / 10;
+        j = position % 10;
+        i -= 1;
+        j += 1;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+        {
+            if ((chess_board[i][j] % 10) != type % 10 && chess_board[i][j] % 10 != 0)
+            {
+                allowed_moves.push_back(((i * 10) + j));
+                break;
+            }
+            allowed_moves.push_back(((i * 10) + j));
+            i -= 1;
+            j += 1;
+        }
+        i = position / 10;
+        j = position % 10;
+        i += 1;
+        j -= 1;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+        {
+            if ((chess_board[i][j] % 10) != type % 10 && chess_board[i][j] % 10 != 0)
+            {
+                allowed_moves.push_back(((i * 10) + j));
+                break;
+            }
+            allowed_moves.push_back(((i * 10) + j));
+            i += 1;
+            j -= 1;
+        }
+        i = position / 10;
+        j = position % 10;
+        i += 1;
+        j -= 1;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+        {
+            if ((chess_board[i][j] % 10) != type % 10 && chess_board[i][j] % 10 != 0)
+            {
+                allowed_moves.push_back(((i * 10) + j));
+                break;
+            }
+            allowed_moves.push_back(((i * 10) + j));
+            i -= 1;
+            j -= 1;
         }
     }
 
+    if (type == 31 || type == 32)
+    {
+
+        i += 2;
+        j += 1;
+        if (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+            allowed_moves.push_back(((i * 10) + j));
+
+        i = position / 10;
+        j = position % 10;
+        i += 2;
+        j -= 1;
+        if (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+            allowed_moves.push_back(((i * 10) + j));
+
+        i = position / 10;
+        j = position % 10;
+        i -= 2;
+        j += 1;
+        if (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+            allowed_moves.push_back(((i * 10) + j));
+
+        i = position / 10;
+        j = position % 10;
+        i -= 2;
+        j -= 1;
+        if (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+            allowed_moves.push_back(((i * 10) + j));
+
+        i = position / 10;
+        j = position % 10;
+        i += 1;
+        j += 2;
+        if (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+            allowed_moves.push_back(((i * 10) + j));
+
+        i = position / 10;
+        j = position % 10;
+        i -= 1;
+        j += 2;
+        if (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+            allowed_moves.push_back(((i * 10) + j));
+
+        i = position / 10;
+        j = position % 10;
+        i += 1;
+        j -= 2;
+        if (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+            allowed_moves.push_back(((i * 10) + j));
+
+        i = position / 10;
+        j = position % 10;
+        i -= 1;
+        j -= 2;
+        if (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+            allowed_moves.push_back(((i * 10) + j));
+    }
+
+    if (type == 22 || type == 21)
+    {
+        i += 1;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+        {
+            if ((chess_board[i][j] % 10) != type % 10 && chess_board[i][j] % 10 != 0)
+            {
+                allowed_moves.push_back(((i * 10) + j));
+                break;
+            }
+            allowed_moves.push_back(((i * 10) + j));
+            i += 1;
+        }
+        i = position / 10;
+        j = position % 10;
+        i -= 1;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+        {
+            if ((chess_board[i][j] % 10) != type % 10 && chess_board[i][j] % 10 != 0)
+            {
+                allowed_moves.push_back(((i * 10) + j));
+                break;
+            }
+            allowed_moves.push_back(((i * 10) + j));
+            i -= 1;
+        }
+        i = position / 10;
+        j = position % 10;
+        j += 1;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+        {
+            if ((chess_board[i][j] % 10) != type % 10 && chess_board[i][j] % 10 != 0)
+            {
+                allowed_moves.push_back(((i * 10) + j));
+                break;
+            }
+            allowed_moves.push_back(((i * 10) + j));
+            j += 1;
+        }
+        i = position / 10;
+        j = position % 10;
+        j -= 1;
+        while (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+        {
+            if ((chess_board[i][j] % 10) != type % 10 && chess_board[i][j] % 10 != 0)
+            {
+                allowed_moves.push_back(((i * 10) + j));
+                break;
+            }
+            allowed_moves.push_back(((i * 10) + j));
+            j -= 1;
+        }
+        
+    }
+
+    if (type == 52 || type == 51)
+    {
+        std::vector<int> inserted_moves = find_moves(chess_board, ((i * 10) + j), 22);
+        allowed_moves.insert(allowed_moves.end(), inserted_moves.begin(), inserted_moves.end());
+        inserted_moves = find_moves(chess_board, ((i * 10) + j), 42);
+        allowed_moves.insert(allowed_moves.end(), inserted_moves.begin(), inserted_moves.end());
+        for (auto &p : allowed_moves)
+            std::cout << p << " ";
+        std::cout << std::endl;
+    }
+
+    if (type == 62 || type == 61)
+    {
+        i += 1;
+        if (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+            allowed_moves.push_back(((i * 10) + j));
+
+        i = position / 10;
+        j = position % 10;
+        i -= 1;
+        if (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+            allowed_moves.push_back(((i * 10) + j));
+
+        i = position / 10;
+        j = position % 10;
+        j += 1;
+        if (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+            allowed_moves.push_back(((i * 10) + j));
+
+        i = position / 10;
+        j = position % 10;
+        j -= 1;
+        if (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+            allowed_moves.push_back(((i * 10) + j));
+
+        i = position / 10;
+        j = position % 10;
+        i += 1;
+        j += 1;
+        if (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+            allowed_moves.push_back(((i * 10) + j));
+
+        i = position / 10;
+        j = position % 10;
+        i -= 1;
+        j += 1;
+        if (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+            allowed_moves.push_back(((i * 10) + j));
+
+        i = position / 10;
+        j = position % 10;
+        i += 1;
+        j -= 1;
+        if (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+            allowed_moves.push_back(((i * 10) + j));
+
+        i = position / 10;
+        j = position % 10;
+        i -= 1;
+        j -= 1;
+        if (i < 8 && j < 8 && i >= 0 && j >= 0 && ((chess_board[i][j] % 10) != type % 10))
+            allowed_moves.push_back(((i * 10) + j));
+    }
+    return allowed_moves;
 }
 
-void find_moves(std::vector<int> &allowed_moves, std::vector<std::vector<int>> chess_board,int position,int type){
-        int i=position/10;
-        int j=position%10;
-
-        if(type==10){
-            if(i>0)i++;
-            allowed_moves.push_back(((i*10)+j));
+void check(std::unordered_map<int, std::vector<int>> &allowed_moves, std::vector<std::vector<int>> &chess_board)
+{
+    for (int i = 0; i < chess_board.size(); i++)
+    {
+        for (int j = 0; j < chess_board[0].size(); j++)
+        {
+            if (chess_board[i][j] == 0)
+                continue;
+            allowed_moves[chess_board[i][j]] = find_moves(chess_board, ((i * 10) + j), (chess_board[i][j]));
+            // std::vector<int> unallowed_moves=allowed_moves[chess_board[i][j]];
+            // std::cout<< chess_board[i][j]<<": "<<unallowed_moves.size()<<std::endl;
         }
-        if(type==11){
-            if(i<8)i--;
-            allowed_moves.push_back(((i*10)+j));
-        }
-
-        if( type==40 || type== 41){
-            while(i<8 && i<8 && i>0 && j>0 && ((chess_board[i][j]%10)!=type%10)){
-                i+=1;
-                j+=1;
-                allowed_moves.push_back(((i*10)+j));
-            }
-            i=position/10;
-            j=position%10;
-            while(i<8 && i<8 && i>0 && j>0 && ((chess_board[i][j]%10)!=type%10)){
-                i-=1;
-                j+=1;
-                allowed_moves.push_back(((i*10)+j));
-            }
-            i=position/10;
-            j=position%10;
-            while(i<8 && i<8 && i>0 && j>0 && ((chess_board[i][j]%10)!=type%10)){
-                i+=1;
-                j-=1;
-                allowed_moves.push_back(((i*10)+j));
-            }
-            i=position/10;
-            j=position%10;
-            while(i<8 && i<8 && i>0 && j>0 && ((chess_board[i][j]%10)!=type%10)){
-                i-=1;
-                j-=1;
-                allowed_moves.push_back(((i*10)+j));
-            }
-        }
-
-        if( type==30 || type== 31){
-            i+=2;
-            j+=1;
-            if(i<8 && i<8 && i>0 && j>0 && ((chess_board[i][j]%10)!=type%10))allowed_moves.push_back(((i*10)+j));
-
-
-            i=position/10;
-            j=position%10;
-            i+=2;
-            j-=1;
-            if(i<8 && i<8 && i>0 && j>0 && ((chess_board[i][j]%10)!=type%10))allowed_moves.push_back(((i*10)+j));
-
-
-            i=position/10;
-            j=position%10;
-            i-=2;
-            j+=1;
-            if(i<8 && i<8 && i>0 && j>0 && ((chess_board[i][j]%10)!=type%10))allowed_moves.push_back(((i*10)+j));
-
-
-            i=position/10;
-            j=position%10;
-            i-=2;
-            j-=1;
-            if(i<8 && i<8 && i>0 && j>0 && ((chess_board[i][j]%10)!=type%10))allowed_moves.push_back(((i*10)+j));
-
-
-            i=position/10;
-            j=position%10;
-            i+=1;
-            j+=2;
-            if(i<8 && i<8 && i>0 && j>0 && ((chess_board[i][j]%10)!=type%10))allowed_moves.push_back(((i*10)+j));
-
-
-            i=position/10;
-            j=position%10;
-            i-=1;
-            j+=2;
-            if(i<8 && i<8 && i>0 && j>0 && ((chess_board[i][j]%10)!=type%10))allowed_moves.push_back(((i*10)+j));
-
-
-            i=position/10;
-            j=position%10;
-            i+=1;
-            j-=2;
-            if(i<8 && i<8 && i>0 && j>0 && ((chess_board[i][j]%10)!=type%10))allowed_moves.push_back(((i*10)+j));
-
-
-            i=position/10;
-            j=position%10;
-            i-=1;
-            j-=2;
-            if(i<8 && i<8 && i>0 && j>0 && ((chess_board[i][j]%10)!=type%10))allowed_moves.push_back(((i*10)+j));
-
-        }
-
-        if( type==20 || type== 21){
-            while(i<8 && i<8 && i>0 && j>0 && ((chess_board[i][j]%10)!=type%10)){
-                i+=1;
-                allowed_moves.push_back(((i*10)+j));
-            }
-            i=position/10;
-            j=position%10;
-            while(i<8 && i<8 && i>0 && j>0 && ((chess_board[i][j]%10)!=type%10)){
-                i-=1;
-                allowed_moves.push_back(((i*10)+j));
-            }
-            i=position/10;
-            j=position%10;
-            while(i<8 && i<8 && i>0 && j>0 && ((chess_board[i][j]%10)!=type%10)){
-                j+=1;
-                allowed_moves.push_back(((i*10)+j));
-            }
-            i=position/10;
-            j=position%10;
-            while(i<8 && i<8 && i>0 && j>0 && ((chess_board[i][j]%10)!=type%10)){
-                j-=1;
-                allowed_moves.push_back(((i*10)+j));
-            }
-        }
-
-        if(type==50 || type== 51){
-            find_moves();
-        }
+    }
+    // for(auto &p : allowed_moves[51])std::cout<< p<< " ";
+    // std::cout<<std::endl;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,38 @@
-#include "headers.hpp"
+#include "allowed_moves.hpp"
 
-int main(){
-    std::unordered_map<int,std::string> chess_pieces;
-    chess_pieces [1] = "pawns";
-    chess_pieces [2] = "Rook";
-    chess_pieces [3] = "knigth";
-    chess_pieces [4] = "bishop";
-    chess_pieces [5] = "queen";
-    chess_pieces [6] = "king";
-
-    std::vector<std::vector<int>> chess_board ={{20,30,40,50,60,40,30,20},
-                                                {10,10,10,10,10,10,10,10},
-                                                {00,00,00,00,00,00,00,00},
-                                                {00,00,00,00,00,00,00,00},
-                                                {00,00,00,00,00,00,00,00},
-                                                {00,00,00,00,00,00,00,00},
-                                                {11,11,11,11,11,11,11,11},
-                                                {21,31,41,51,61,41,31,21}
-                                                };
+int main()
+{
+    std::unordered_map<int, std::string> chess_pieces;
+    std::unordered_map<int, std::vector<int>> allowed_moves;
+    chess_pieces[1] = "pawns";
+    chess_pieces[2] = "Rook";
+    chess_pieces[3] = "knigth";
+    chess_pieces[4] = "bishop";
+    chess_pieces[5] = "queen";
+    chess_pieces[6] = "king";
     
-    for(int i=0;i<chess_board.size();i++){
-        for(int j=0;j<chess_board[0].size();j++){
-            std::cout<<chess_board[i][j]<<" ";
+    std::vector<std::vector<int>> chess_board = {
+    //   0   1   2   3   4   5   6   7
+        {22, 32, 42, 52, 62, 42, 32, 22}, // 0
+        {12, 12, 12, 12, 12, 12, 12, 12}, // 1
+        {00, 00, 00, 00, 00, 00, 00, 00}, // 2
+        {00, 00, 00, 52, 00, 00, 00, 00}, // 3
+        {00, 00, 00, 00, 00, 00, 00, 00}, // 4
+        {00, 00, 00, 00, 00, 00, 00, 00}, // 5
+        {11, 11, 11, 11, 11, 11, 11, 11}, // 6
+        {21, 31, 41, 51, 61, 41, 31, 21}  // 7
+    };
+
+    for (int i = 0; i < chess_board.size(); i++)
+    {
+        for (int j = 0; j < chess_board[0].size(); j++)
+        {
+            std::cout << chess_board[i][j] << " ";
         }
-        std::cout<<std::endl;
+        std::cout << std::endl;
     }
-
-
+    // std::cout<<allowed_moves.size();
+    check(allowed_moves, chess_board);
+    std::cout << allowed_moves.size();
+    // for(auto &p : allowed_moves[31])std::cout<< p<< " "<<std::endl;
 }
